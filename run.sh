f9b5565b8e22873acdd3d9513f04212dbbc22ec4
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2019-11-29 13:22:54 +0200

    +modif

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ nohup docker run --rm -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /home/andrei/Documents:/home/andrei/Documents \
     -v /home/andrei/Downloads:/home/andrei/Downloads \
-    -v "/home/andrei/BI Python/Output/multigames":"/home/andrei/BI Python/Output/multigames" \
+    -v "/home/andrei/BI Python/Output/":"/home/andrei/BI Python/Output/" \
     libreoffice-docker:latest &
